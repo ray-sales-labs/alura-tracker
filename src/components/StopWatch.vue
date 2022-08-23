e95807043d7421b8fd7a0e17b9ed23afa77ba255
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-23 19:22:47 -0300

    [REFACTOR] separate stopwatch component

---
+++ src/components/StopWatch.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <strong> {{ elapsedTime }} </strong>
+  </section>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'StopWatch',
+    props: {
+      timeInSeconds: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      elapsedTime () : string {
+        return new Date(this.timeInSeconds * 1000).toISOString().substring(11,19)
+      }
+    },
+  })
+</script>
