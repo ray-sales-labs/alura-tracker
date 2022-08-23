28fd6c160c2386391e29f08f8a3b791a0dc0eee4
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-23 19:22:07 -0300

    [REFACTOR] separate button component

---
+++ src/components/ButtonForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <button class="button" @click="clicked" :disabled="disabled">
+      <span class="icon">
+        <i :class="icon">
+        
+        </i>
+      </span>
+      <span>{{ label }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'ButtonForm',
+    emits: [
+      'Clicked'
+    ],
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      icon: {
+        type: String,
+        default: ''
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      clicked(): void {
+        this.$emit('Clicked')
+      }
+    }
+  })
+</script>
