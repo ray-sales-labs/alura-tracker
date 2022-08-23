3f07c5f0749f2c316b064e03c5278695bd6bef4f
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-22 22:42:57 -0300

    [FEAT] BarraLateral component createed

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="Logo Alura Tracker">
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'BarraLateral'
+  })
+</script>
+
+<style scoped>
+  header {
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    padding: 1rem;
+  }
+
+  header h1 img {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 768px) {
+    header {
+      padding: 2.5rem;
+      height: auto;
+    }
+  }
+
+</style>
