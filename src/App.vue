ff34fe8c1f2abeea2ec62718a272a1d6f7ca3016
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-23 22:41:30 -0300

    [FEAT] implements dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'dark-mode': darkModeActive }">
     <div class="column is-one-quarter">
-      <SideBar />
+      <SideBar @changeTheme="changeTheme"/>
     </div>
-    <div class="column is-three-quarters">
+    <div class="column is-three-quarters content">
       <TaskForm @OnSaveTask="saveTask" />
       <!-- Task list -->
       <div class="list">
+        <TaskBox v-if="withoutTasks">
+          Você não está muito produtivo hoje :(
+        </TaskBox>
         <TaskItem v-for="(task, index) in tasks" :key="index" :task="task"/>
       </div>
     </div>
@@ -15,9 +18,10 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import SideBar from './components/SideBar.vue';
-import TaskForm from './components/TaskForm.vue';
+import SideBar from './components/SideBar.vue'
+import TaskForm from './components/TaskForm.vue'
 import TaskItem from './components/TaskItem.vue'
+import TaskBox from './components/TaskBox.vue'
 import ITaskItem from './interfaces/ITaskItem'
 
 export default defineComponent({
@@ -25,16 +29,26 @@ export default defineComponent({
     components: {
     SideBar,
     TaskForm,
-    TaskItem
+    TaskItem,
+    TaskBox
   },
   data() {
     return {
-      tasks: [] as ITaskItem[]
+      tasks: [] as ITaskItem[],
+      darkModeActive: false
+    }
+  },
+  computed: {
+    withoutTasks(): boolean {
+      return this.tasks.length === 0
     }
   },
   methods: {
     saveTask(task: ITaskItem): void {
       this.tasks.push(task)
+    },
+    changeTheme(active: boolean): void {
+      this.darkModeActive = active
     }
   }
 });
@@ -43,5 +57,19 @@ export default defineComponent({
 <style>
 .list {
   padding: 1.25rem;
+}
+
+main {
+  --bg-primary: #fff;
+  --text-primary: #000;
+}
+
+main.dark-mode {
+  --bg-primary: #2b2d42;
+  --text-primary: #ddd;
+}
+
+.content {
+  background-color: var(--bg-primary);
 }
 </style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,15 +3,35 @@
     <h1>
       <img src="../assets/logo.png" alt="Logo Alura Tracker">
     </h1>
+    <ButtonForm :label="statusButton" @clicked="changeTheme" />
   </header>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
+import ButtonForm from './ButtonForm.vue';
 
   export default defineComponent({
-    name: 'SideBar'
-  })
+    name: "SideBar",
+    emits: ['ChangeTheme'],
+    components: { ButtonForm },
+    data(){
+      return {
+        darkModeActive: false
+      }
+    },
+    computed: {
+      statusButton (): string {
+        return this.darkModeActive ? 'Desativar modo escuro' : 'Ativar modo escuro'
+      }
+    },
+    methods: {
+      changeTheme () {
+        this.darkModeActive = !this.darkModeActive
+        this.$emit('ChangeTheme', this.darkModeActive)
+      }
+    }
+})
 </script>
 
 <style scoped>
@@ -20,6 +40,7 @@
     width: 100%;
     height: 100vh;
     padding: 1rem;
+    text-align: center;
   }
 
   @media only screen and (max-width: 768px) {
