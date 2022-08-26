273f2033f8f6a67735f9f4ca2364d94d3bc46441
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-25 22:20:22 -0300

    [FEAT] config router

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
+import TasksView from '../views/TasksView.vue'
+import ProjectsView from '../views/ProjectsView.vue'
+
+const routes: RouteRecordRaw[] = [
+  { 
+    path: '/', 
+    name: 'Tasks', 
+    component: TasksView 
+  },
+  { 
+    path: '/projects', 
+    name: 'Projects', 
+    component: ProjectsView 
+  }
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+
+export default router
