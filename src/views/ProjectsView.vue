46e0729331559dca9e98474c4dece7a0f18f8fee
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-25 22:20:53 -0300

    [FEAT] project component created

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="projects">
+    <h1 class="title">Projects</h1>
+    <form @submit.prevent="save">
+      <div class="field">
+        <label for="projectName" class="label">
+          Nome do Projeto
+        </label>
+        <input type="text" class="input" v-model="projectName" id="nameProject">
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'ProjectsView'
+  })
+</script>
