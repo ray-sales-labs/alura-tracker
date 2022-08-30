1211ce3fd8be0d8a11402584055ef384a78a1153
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-30 20:12:00 -0300

    [FEAT] new routes added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import TasksView from '../views/TasksView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
-
+import ProjectsForm from '../views/projects/ProjectsForm.vue'
 const routes: RouteRecordRaw[] = [
   { 
     path: '/', 
@@ -12,6 +12,17 @@ const routes: RouteRecordRaw[] = [
     path: '/projects', 
     name: 'Projects', 
     component: ProjectsView 
+  },
+  { 
+    path: '/projects/new', 
+    name: 'New Project', 
+    component: ProjectsForm 
+  },
+  { 
+    path: '/projects/:id', 
+    name: 'Edit Project', 
+    component: ProjectsForm,
+    props: true
   }
 ]
 const router = createRouter({
