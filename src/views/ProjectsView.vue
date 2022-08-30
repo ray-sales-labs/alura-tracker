8cba6f972bcc301d570edd00df26d1ff4cda2304
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-29 22:17:44 -0300

    [FEAT] projects table added

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -12,27 +12,52 @@
         <button class="button" type="submit">Salvar</button>
       </div>
     </form>
+    <table class="table is-full-width">
+      <thead>
+        <tr>
+          <th>
+            id
+          </th>
+          <th>
+            Name
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>
+            {{ project.id }}
+          </td>
+          <td>
+            {{ project.name }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { computed, defineComponent } from 'vue'
   import IProjectItem from '../interfaces/IProjectItem'
+  import {useStore} from '@/store'
   export default defineComponent({
     name: 'ProjectsView',
     data(){
       return {
         projectName: '',
-        projects: [] as IProjectItem[]
       }
     },
     methods: {
       save() {
-        const project: IProjectItem = {
-          name: this.projectName,
-          id: new Date().toISOString()
-        }
-        this.projects.push(project)
+        this.store.commit('ADD_PROJECT', this.projectName)
         this.projectName = ''
+      }
+    },
+    setup() {
+      const store = useStore()
+      return {
+        store,
+        projects: computed(() => store.state.projects)
       }
     }
   })
