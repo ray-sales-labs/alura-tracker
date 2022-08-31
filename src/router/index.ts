ae3975b9ae7505722aabf02830c2ad939b8acb59
Rayssa Sales <rayssa.gabriela@icloud.com>
2022-08-31 19:45:33 -0300

    [FEAT] create new routes and project list component

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,27 +2,34 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
 import TasksView from '../views/TasksView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
 import ProjectsForm from '../views/projects/ProjectsForm.vue'
+import ProjectsList from '../views/projects/ProjectsList.vue'
+
 const routes: RouteRecordRaw[] = [
   { 
     path: '/', 
     name: 'Tasks', 
     component: TasksView 
   },
-  { 
-    path: '/projects', 
-    name: 'Projects', 
-    component: ProjectsView 
-  },
-  { 
-    path: '/projects/new', 
-    name: 'New Project', 
-    component: ProjectsForm 
-  },
-  { 
-    path: '/projects/:id', 
-    name: 'Edit Project', 
-    component: ProjectsForm,
-    props: true
+  {
+    path: '/projects',
+    component: ProjectsView,
+    children: [
+      {
+        path: '',
+        component: ProjectsList,
+      },
+      { 
+        path: 'new', 
+        name: 'New Project', 
+        component: ProjectsForm 
+      },
+      { 
+        path: ':id', 
+        name: 'Edit Project', 
+        component: ProjectsForm,
+        props: true
+      }
+    ]
   }
 ]
 const router = createRouter({
---
+++ src/views/projects/ProjectsList.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class="projects">
+    <h1 class="title">Projects</h1>
+    <router-link to="/projects/new" class="button mb-5">
+      <span class="icon is-small">
+        <i class="fas fa-plus">
+
+        </i>
+      </span>
+      <span>New Project</span>
+    </router-link>
+    <table class="table is-full-width">
+      <thead>
+        <tr>
+          <th>
+            id
+          </th>
+          <th>
+            Name
+          </th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>
+            {{ project.id }}
+          </td>
+          <td>
+            {{ project.name }}
+          </td>
+          <td>
+            <router-link :to="`/projects/${project.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt">
+
+                </i>
+              </span>
+            </router-link>
+            <button class="button ml-2 is-danger" @click="deleteItem(project.id)">
+              <span class="icon is-small">
+                <i class="fas fa-trash"></i>
+              </span>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+<script lang="ts">
+  import { computed, defineComponent } from 'vue'
+  import {useStore} from '@/store'
+  import { DELETE_PROJECT } from '@/store/mutations_type'
+  
+  export default defineComponent({
+    name: 'ProjectsList',
+    methods: {
+      deleteItem(id: string) {
+        this.store.commit(DELETE_PROJECT, id)
+      }
+    },
+    setup() {
+      const store = useStore()
+      return {
+        store,
+        projects: computed(() => store.state.projects)
+      }
+    }
+  })
+</script>
+<style scoped>
+  .projects {
+    padding: 1.25rem;
+  }
+</style>
